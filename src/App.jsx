f6e42769030236c82709f558038a5701277670ab
Israel Godinez B <isra.gb47@gmail.com>
2023-08-19 12:35:57 -0600

    before creating item context

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,8 @@
 import { BrowserRouter } from 'react-router-dom'
 import RoutesIndex from './routes/Index'
 import './App.css'
-import Footer from './components/Footer'
-import Navbar from './components/Navbar'
+import Footer from './components/Footer/Footer'
+import Navbar from './components/Navbar/Navbar'
 
 function App () {
   return (
--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,5 +1,5 @@
 import React, { useState, useEffect } from 'react'
-import Item from '../components/Item'
+import Item from '../components/Item/Item'
 import { Link } from 'react-router-dom'
 
 const Home = () => {
